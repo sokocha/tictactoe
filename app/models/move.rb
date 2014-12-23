d0bbd1eb8734e77cadb0d9b7b1f6c4987e0929b2
Sadiq Okocha <sokocha@gmail.com>
2014-12-23 22:01:16 +0000

    Adds better error messages and a bit of css

--- app/models/move.rb
+++ app/models/move.rb
@@ -4,22 +4,27 @@ class Move < ActiveRecord::Base
   validate :if_match_active, on: :create
   validate :is_users_turn
   validate :is_within_range?
-
-  after_create :check_win
+  validate :match_winner, on: :create
 
   def if_match_active
-    errors.add(:match_id, 'not active') if !match.is_match_active?
+    errors.add(:base, 'This match is no longer active') if !match.is_match_active?
   end
 
-  def check_win
-    match.match_won
+  # def check_win
+  #   match.match_won
+  # end
+
+  def match_winner
+    errors.add(:base, 'There is a winner. Start a new game') if match.winner_id != nil
   end
 
-
+  
 
   def is_users_turn
-    errors.add(:base, "Is not this user's turn") if match.whose_turn.id != user.id
+    errors.add(:base, "It is not your turn") if match.whose_turn.id != user.id
   end
+
+
 
   def is_within_range?
       errors.add(:base, "The index is not in range") unless match.starting_board.include?(square_id)
